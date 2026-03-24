b1aad5bf450e29069adca4a51be1a1e9b3541795
hellowang001 <tiamwang@163.com>
2026-03-24 20:48:13 +0800

    add map

--- learn/programs/learn/src/lib.rs
+++ learn/programs/learn/src/lib.rs
@@ -6,11 +6,38 @@ declare_id!("8sm8zjwUwhvBLxhv1RHd64fQvdAasxoE3j57NrqSxJdr");
 pub mod learn {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings  test from 42131 1111: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>,key:u64) -> Result<()> {
+        Ok(())
+    }
+    pub fn set(ctx: Context<Set>, key:u64) -> Result<()> {
+        ctx.accounts.val.value = key;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(key:u64)]
+pub struct Initialize<'info> {
+    #[account(init,
+            payer=signer,
+            space=size_of::<Val>()+8,
+            seeds=[&key.to_le_bytes().as_ref()],//seeds 参数 key 可以看作是一个类似于 Solidity 构造形式的“键”：
+            bump)]
+    val:Account<'info, Val>,
+
+    #[account(mut)]
+    signer: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct Val {
+    value:u64,
+}
+#[derive(Accounts)]
+#[instruction(key:u64)]
+pub struct Set<'info> {
+    #[account(mut)]
+    val:Account<'info, Val>,
+}
--- learn/tests/learn.ts
+++ learn/tests/learn.ts
@@ -7,10 +7,41 @@ describe("learn", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.learn as Program<Learn>;
-
-  it("Is initialized!", async () => {
+  /*
+  it("初始化映射 !", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const  key = new anchor.BN(42);
+    const seeds =[key.toArrayLike(Buffer,"le",8)]
+    let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
+        seeds,
+        program.programId
+    )
+    const tx = await program.methods.initialize(key).accounts({val:valueAccount}).rpc();
     console.log("Your transaction signature", tx);
   });
+
+   */
+
+  it("初始化映射并且设置 !", async () => {
+    // Add your test here.
+    const  key = new anchor.BN(42);
+    const value =new anchor.BN(1337);
+    const seeds =[key.toArrayLike(Buffer,"le",8)]
+    let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
+        seeds,
+        program.programId
+    )[0];
+    // 初始化initialize 账户，把 key 42 放在了valueAccount的 seeds 中
+    await program.methods.initialize(key).accounts({val:valueAccount}).rpc();
+    // 设置账户
+    // 修改了这个账户的 seeds
+    await program.methods.set(value).accounts({val:valueAccount}).rpc();
+
+    // 读取账户
+    let result = await program.account.val.fetch(valueAccount);
+
+    console.log(`值 ${result.value} 被存储在 ${valueAccount.toBase58()}`);
+  });
 });
+// solana-test-validator
+// anchor test --skip-local-validator
